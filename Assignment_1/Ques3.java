6a5a579a0fdcc2b814c308f30c9383a6987ef4cf
vaibhav1620 <43521611+vaibhav1620@users.noreply.github.com>
2020-08-21 17:18:43 +0530

    Update Ques3.java

--- Assignment_1/Ques3.java
+++ Assignment_1/Ques3.java
@@ -1,98 +1,57 @@
-import java.util.*; 
-
-class Ques3 {
-
-    static class Edge {
-        int src, dest, weight;
+import java.util.*;
+public class Ques3{
+	static class Edge{
+		int src, dest, cost;
+		Edge(int s, int d, int c){
+			src = s;
+			dest = d;
+			cost = c;
+		}
+	}
+	public static void print(String msg){
+		System.out.print(msg);
+	}
+	public static String bellmanFord(ArrayList<Edge> edges, int n, int src, int dest){
+		int m = edges.size();
+		int dist[] = new int[n];
+		Arrays.fill(dist,Integer.MAX_VALUE);
+		dist[src] = 0;
+		for(int i = 1;i<n;i++){
+			for(Edge edge:edges){
+				int u = edge.src;
+				int v = edge.dest;
+				int w = edge.cost;
+				if(dist[u]!=Integer.MAX_VALUE&&dist[v]>dist[u]+w){
+					dist[v] = dist[u]+w;
+				}
+			}
+		}
+		for(Edge edge:edges){
+			int u = edge.src;
+			int v = edge.dest;
+			int w = edge.cost;
+			if(dist[u] != Integer.MAX_VALUE && dist[u]+w<dist[v]){
+				return "Graph contains negative weight cycle";
+			}
+		}
+		return dist[dest]==Integer.MAX_VALUE?"No path":String.valueOf(dist[dest]);
+	}
+    public static void main(String args[]){
+    	Scanner sc = new Scanner(System.in);
+    	print("Enter no. of vertices in graph:\n");
+    	int n = sc.nextInt();
+    	print("Enter no. of edges in graph:\n");
+    	int m = sc.nextInt();
+    	ArrayList<Edge> edges = new ArrayList<Edge>(m);
+    	print("Enter the edges in the format:\nSource Destination Cost\n");
+    	for(int i = 0;i<m;i++){
+    		edges.add(new Edge(sc.nextInt(),sc.nextInt(),sc.nextInt()));
+    	}
+    	print("Enter source vertex:\n");
+    	int src = sc.nextInt();
+    	print("Enter destination vertex:\n");
+    	int dest = sc.nextInt();
+    	print(bellmanFord(edges,n,src,dest)+"\n");
     }
 
-    static class Graph {
-
-        int V, E;
-        Edge edge[];
-
-    }
-
-    static Graph createGraph(int V, int E) {
-        Graph graph = new Graph();
-        graph.V = V;
-        graph.E = E;
-        graph.edge = new Edge[graph.E];
-
-        for (int i = 0; i < graph.E; i++) {
-            graph.edge[i] = new Edge();
-        }
-
-        return graph;
-    }
-
-    static boolean isNegCycleBellmanFord(Graph graph, int src) {
-        int V = graph.V;
-        int E = graph.E;
-        int[] dist = new int[V];
-        for (int i = 0; i < V; i++)
-            dist[i] = Integer.MAX_VALUE;
-        dist[src] = 0;
-        for (int i = 1; i <= V - 1; i++) {
-            for (int j = 0; j < E; j++) {
-                int u = graph.edge[j].src;
-                int v = graph.edge[j].dest;
-                int weight = graph.edge[j].weight;
-                if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v])
-                    dist[v] = dist[u] + weight;
-            }
-        }
-
-        for (int i = 0; i < E; i++) {
-            int u = graph.edge[i].src;
-            int v = graph.edge[i].dest;
-            int weight = graph.edge[i].weight;
-            if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v])
-                return true;
-        }
-
-        return false;
-    }
-
-    public static void main(String[] args) {
-        int V = 5, E = 8;
-        Graph graph = createGraph(V, E);
-
-        graph.edge[0].src = 0;
-        graph.edge[0].dest = 1;
-        graph.edge[0].weight = -1;
-
-        graph.edge[1].src = 0;
-        graph.edge[1].dest = 2;
-        graph.edge[1].weight = 4;
-
-        graph.edge[2].src = 1;
-        graph.edge[2].dest = 2;
-        graph.edge[2].weight = 3;
-
-        graph.edge[3].src = 1;
-        graph.edge[3].dest = 3;
-        graph.edge[3].weight = 2;
-
-        graph.edge[4].src = 1;
-        graph.edge[4].dest = 4;
-        graph.edge[4].weight = 2;
-
-        graph.edge[5].src = 3;
-        graph.edge[5].dest = 2;
-        graph.edge[5].weight = 5;
-
-        graph.edge[6].src = 3;
-        graph.edge[6].dest = 1;
-        graph.edge[6].weight = 1;
-
-        graph.edge[7].src = 4;
-        graph.edge[7].dest = 3;
-        graph.edge[7].weight = -3;
-
-        if (isNegCycleBellmanFord(graph, 0))
-            System.out.println("Yes");
-        else
-            System.out.println("No");
-    }
 }
